9485996c2dfbc5b7d4e00025172376e541c20059
Pedro <pedroo.bezerra@gmail.com>
2020-06-16 01:32:44 -0300

    first challenge bootcamp

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,24 +10,57 @@ app.use(cors());
 
 const repositories = [];
 
+const validateId = (request, response, next) => 
+{
+    const {id} = request.params;
+    response.locals.repositoryIndex = repositories.findIndex(repo => repo.id === id);
+
+    if (response.locals.repositoryIndex < 0) return response.status(400).send("Repository not found.")
+    
+    return next()
+}
+
+app.use('/repositories/:id', validateId)
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const {title, url, techs} = request.body; 
+  const repository = {id: uuid(), title, url, techs, likes:0};
+
+  repositories.push(repository);
+
+  response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params;
+  const repositoryIndex = response.locals.repositoryIndex
+
+  const {title, url, techs} = request.body;
+
+  const repository = {id, title, url, techs, likes : 0} 
+
+  repositories[repositoryIndex] = repository
+
+  response.json(repository)
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const repositoryIndex = response.locals.repositoryIndex
+
+  repositories.splice(repositoryIndex,1);
+  return response.status(204).send()
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const repositoryIndex = response.locals.repositoryIndex
+
+  repositories[repositoryIndex].likes++   
+
+  response.json(repositories[repositoryIndex]);
 });
 
 module.exports = app;
